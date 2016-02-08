49dc9ce792b5d1b4d1f8192d454e9b1e448ccd75
Akshay Joshi <joshiaks91@gmail.com>
2016-02-08 10:45:41 -0800

    Random Number Generated

--- GameDie.cpp
+++ GameDie.cpp
@@ -1,11 +1,29 @@
+//I have successfully cloned and pushed
+#include <stdlib.h>
+#include <iostream>
 #include <cstdlib>
-
+#include <time.h>
+#include <random>
+#include <ctime>
+using std::cout;
+using std::string;
+using std::endl;
+using namespace std;
 //class constructor that seeds the random number generator
 GameDie::GameDie()
 {
+roll();
 }
 
-//generate a random number between 1-6 (inclusive) and return it
-int GameDie::roll()
+//generate a random number between 1-6 (inclusive) and display it
+void GameDie::roll()
 {
+
+int number;
+std::default_random_engine engi;
+std::random_device aj;
+std::uniform_int_distribution<int> rand(4,20);
+engi.seed(aj());
+number=rand(engi);
+std::cout<<"This is the random number between 4 to 20 : " <<number<<std::endl;
 }
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "GameDie.h"
+using namespace std;
+int main()
+{
+GameDie g;
+return 0;
+}
